19d1d84fa5d50ef4d6bca95b4b7f64c45d5970eb
Alan Malbos <alanmalbos@gmail.com>
2019-06-27 11:07:11 -0300

    Add sharp library

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-//mongoose.connect('mongodb+srv://dbUser:chinelo.0311@cluster0-lmuhb.mongodb.net/test?retryWrites=true&w=majority',{
-//    useNewUrlParser: true,
-//});
+mongoose.connect('mongodb+srv://dbUser:chinelo.0311@cluster0-lmuhb.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+});
 
-mongoose.connect('mongodb://localhost/instaMalbos', {useNewUrlParser: true});
+//mongoose.connect('mongodb://localhost/instaMalbos', {useNewUrlParser: true});
 //C:\Program Files\MongoDB\Server\4.0\data\
 
 app.use(require('./routes'));
