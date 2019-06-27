b4a36d26ef68bd90c5b869b0f0e60c255365d1e2
Alan Malbos <alanmalbos@gmail.com>
2019-06-27 15:01:20 -0300

    Install socket.io

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -39,6 +39,8 @@ module.exports = {
             filename,
         });
 
+        req.io.emit('post', post);
+
         return res.json({
             post
         });
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 //mongoose.connect('mongodb+srv://dbUser:chinelo.0311@cluster0-lmuhb.mongodb.net/test?retryWrites=true&w=majority',{
 //   useNewUrlParser: true,
@@ -11,9 +15,17 @@ const app = express();
 mongoose.connect('mongodb://localhost/instaMalbos', {useNewUrlParser: true});
 //C:\Program Files\MongoDB\Server\4.0\data\
 
+app.use((req, res, next) => {
+    req.io = io;
+
+    next();
+});
+
+app.use(cors());
+
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
 
